15c121882a07bfadb6f6fb2cbbb7baa0a11181de
mcnalj <example@example.com>
2021-04-20 10:23:52 -0400

    Set DEBUG based on environment variables.

--- copacity/settings.py
+++ copacity/settings.py
@@ -126,3 +126,8 @@ STATIC_URL = '/static/'
 # Heroku settings.
 import django_heroku
 django_heroku.settings(locals())
+
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
