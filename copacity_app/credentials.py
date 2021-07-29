d77b088b318e71b574d190743cc3fe54dfb536b5
mcnalj <example@example.com>
2021-07-29 17:12:06 -0400

    updated Twilio credentials

--- copacity_app/credentials.py
+++ copacity_app/credentials.py
@@ -1,4 +1,4 @@
 class Credentials():
     """Keys to Twilio."""
     account_sid = 'AC06ad976fede76e018d6c19aba6ea8bb7'
-    auth_token = '13ee6d0541c01e50e7008cdb6f0484f2'
+    auth_token = 'f6e404e0b4c94b5488e0137191fddcbf'
