48432cff7585af342599c06cac497947e4b68195
Jake McNally <jakemcnally@gmail.com>
2021-08-10 12:53:59 -0400

    Delete credentials.py

--- copacity_app/credentials.py
+++
@@ -1,8 +0,0 @@
-class Credentials():
-    """Keys to Twilio."""
-    account_sid = 'AC06ad976fede76e018d6c19aba6ea8bb7'
-    auth_token = 'f6e404e0b4c94b5488e0137191fddcbf'
-
-class AppPasswordForSMTP():
-    """App password for gmail for password reset email."""
-    app_password = 'hdojancloqokybmr'
